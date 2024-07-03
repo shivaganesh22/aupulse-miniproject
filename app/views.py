dca8d16f54bf9b3832ce817a3bf34264dd2968eb
Dornala Shivaganesh <71806997+shivaganesh22@users.noreply.github.com>
2024-07-03 06:05:15 +0000

    deeface

--- app/views.py
+++ app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from deepface import DeepFace
+#liveness
+import tensorflow as tf
+import cv2, numpy, datetime, pygame, keyboard
+import xlwings as xw
+model = 'liveness.model'
+model = tf.keras.models.load_model(model)
