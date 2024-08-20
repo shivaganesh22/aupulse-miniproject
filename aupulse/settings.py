e7823c1c0e22fac9ab089a8b0bed253a290020c3
Dornala Shivaganesh <71806997+shivaganesh22@users.noreply.github.com>
2024-08-20 09:19:58 +0000

    url

--- aupulse/settings.py
+++ aupulse/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ecp4(8kx+9xj7n0fi@ey87_grcecnqwoqwwu#dd4qry+i7(xmj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 import os
 ALLOWED_HOSTS = ['*']
 
